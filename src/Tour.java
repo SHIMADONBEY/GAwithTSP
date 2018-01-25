f294135f8fb9f1306c5dae40448f52865ffd1eae
SHIMADONBEY <hei96hiraoka@gmail.com>
2018-01-25 12:43:43 +0900

    アルゴリズム変更

--- src/Tour.java
+++ src/Tour.java
@@ -1,102 +1,101 @@
 
 public class Tour {
-    private int[] route;
-    private Map map;
-    private int cityCount;
+	private int[] route;
+	private Map map;
+	private int cityCount;
+	private static double unit = 1000.0;
 
-    /**
-     * @param n 都市数
-     */
-    public Tour(int n) {
-        // TODO 自動生成されたコンストラクター・スタブ
-        cityCount = n;
-        route = new int[n];
-        map = new Map(n);
-    }
+	/**
+	 * @param n 都市数
+	 */
+	public Tour(int n) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		cityCount = n;
+		route = new int[n];
+		map = new Map(n);
+	}
 
-    /**
-     * @param m 地図データ
-     */
-    public Tour(Map m) {
-        cityCount = m.getCityCount();
-        route = new int[cityCount];
-        map = new Map(cityCount);
-        setMap(m);
-    }
+	/**
+	 * @param m 地図データ
+	 */
+	public Tour(Map m) {
+		cityCount = m.getCityCount();
+		route = new int[cityCount];
+		map = new Map(cityCount);
 
-    public int[] getRoute() {
-        return route;
-    }
+		setMap(m);
+	}
 
-    public int getRoute(int p) {
-        return route[p];
-    }
+	public int[] getRoute() {
+		return route;
+	}
 
-    /**
-     * @param route 経路配列
-     */
-    public void setRoute(int[] route) {
-        for (int i = 0; i < cityCount; i++) {
-            this.route[i] = route[i];
-        }
-    }
+	public int getRoute(int p) {
+		return route[p];
+	}
 
-    public Map getMap() {
-        return map;
-    }
+	/**
+	 * @param route 経路配列
+	 */
+	public void setRoute(int[] route) {
+		for (int i = 0; i < cityCount; i++) {
+			this.route[i] = route[i];
+		}
+	}
 
-    /**
-     * @param map 地図データ
-     */
-    public void setMap(Map map) {
-        for (int i = 0; i < cityCount; i++) {
-            this.map.setCity(i, map.getCity(i).x, map.getCity(i).y);
-        }
-    }
+	public Map getMap() {
+		return map;
+	}
 
-    /**
-     * 経路を生成する
-     */
-    public void makeTour() {
-        int i, j;
-        int swp;
-        int cityCount = map.getCityCount();
+	/**
+	 * @param map 地図データ
+	 */
+	public void setMap(Map map) {
+		for (int i = 0; i < cityCount; i++) {
+			this.map.setCity(i, map.getCity(i).x, map.getCity(i).y);
+		}
+		this.map.setEdge(map.getEdge());
+	}
 
-        for (i = 0; i < cityCount; i++) {
-            route[i] = i;
-        }
+	/**
+	 * 経路を生成する
+	 */
+	public void makeTour() {
+		int i, j;
+		int swp;
+		int cityCount = map.getCityCount();
 
-        for (i = 0; i < cityCount; i++) {
-            j = (int) (Math.random() * (cityCount - i)) + i;
-            swp = route[i];
-            route[i] = route[j];
-            route[j] = swp;
-        }
-    }
+		for (i = 0; i < cityCount; i++) {
+			route[i] = i;
+		}
 
-    /**
-     * @param a 都市番号A
-     * @param b 都市番号B
-     * @return AB間のユークリッド距離（1/1000単位）
-     */
-    public double getDistance(int a, int b) {
-        Point cityA = map.getCity(a);
-        Point cityB = map.getCity(b);
+		for (i = 0; i < cityCount; i++) {
+			j = (int) (Math.random() * (cityCount - i)) + i;
+			swp = route[i];
+			route[i] = route[j];
+			route[j] = swp;
+		}
+	}
 
-        return (double) ((int) (Math.sqrt(Math.pow(cityA.x - cityB.x, 2)
-                + Math.pow(cityA.y - cityB.y, 2)) * 1000 + 0.5) / 1000.0);
-    }
+	/**
+	 * @param a 都市番号A
+	 * @param b 都市番号B
+	 * @return AB間のユークリッド距離（1/1000単位）
+	 */
+	public double getDistance(int a, int b) {
+		return (double) ((long) (map.getEdge(a, b) * 1000 + 0.5) / unit);
+	}
 
-    /**
-     * @return 全経路のユークリッド距離総和
-     */
-    public double getDistance() {
-        long total = 0;
-        int cityCount = map.getCityCount();
+	/**
+	 * @return 全経路のユークリッド距離総和
+	 */
+	public double getDistance() {
+		long total = 0;
 
-        for (int i = 0; i < map.getCityCount(); i++) {
-            total += getDistance(route[i], route[(i + 1) % cityCount]) * 1000 + 0.5;
-        }
-        return (double) total / 1000.0;
-    }
+		for (int i = 0; i < map.getCityCount(); i++) {
+			total += getDistance(route[i], route[(i + 1) % cityCount]) * unit + 0.5;
+		}
+
+		return total / unit;
+	}
 }
